e4b4c58f0839799276a7c3e3fe6587a8303f62eb
Henok Urufa <heno.urufa@itechart-group.com>
2022-09-05 11:08:20 +0200

    MS-76
    Create User model

--- MusicHub/config/settings.py
+++ MusicHub/config/settings.py
@@ -16,6 +16,7 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 
+<<<<<<< Updated upstream
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
@@ -63,27 +64,125 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
             ],
+=======
+class Common(Configuration):
+    # Build paths inside the project like this: BASE_DIR / 'subdir'.
+
+    # Quick-start development settings - unsuitable for production
+    # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
+
+    # SECURITY WARNING: keep the secret key used in production secret!
+
+    # SECURITY WARNING: don't run with debug turned on in production!
+    # Set DEBUG to False as a default for safety
+    # https://docs.djangoproject.com/en/dev/ref/settings/#debug
+    DEBUG = False
+    SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
+
+    ALLOWED_HOSTS = ["*"]
+
+    # Email
+    EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+
+    # Application definition
+
+    INSTALLED_APPS = [
+        "django.contrib.admin",
+        "django.contrib.auth",
+        "django.contrib.contenttypes",
+        "django.contrib.sessions",
+        "django.contrib.messages",
+        "django.contrib.staticfiles",
+        # Third party apps
+        "rest_framework",
+        "django_filters",
+        # apps
+        "MusicHub.users",
+    ]
+
+    MIDDLEWARE = [
+        "django.middleware.security.SecurityMiddleware",
+        "django.contrib.sessions.middleware.SessionMiddleware",
+        "django.middleware.common.CommonMiddleware",
+        "django.middleware.csrf.CsrfViewMiddleware",
+        "django.contrib.auth.middleware.AuthenticationMiddleware",
+        "django.contrib.messages.middleware.MessageMiddleware",
+        "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    ]
+
+    ROOT_URLCONF = "MusicHub.urls"
+
+    # Static files (CSS, JavaScript, Images)
+    # https://docs.djangoproject.com/en/4.1/howto/static-files/
+
+    STATIC_ROOT = os.path.normpath(join(os.path.dirname(BASE_DIR), "static"))
+    STATICFILES_DIRS = []
+    STATIC_URL = "/static/"
+    STATICFILES_FINDERS = (
+        "django.contrib.staticfiles.finders.FileSystemFinder",
+        "django.contrib.staticfiles.finders.AppDirectoriesFinder",
+    )
+    # Media files
+    MEDIA_ROOT = join(os.path.dirname(BASE_DIR), "media")
+    MEDIA_URL = "/media/"
+
+    TEMPLATES = [
+        {
+            "BACKEND": "django.template.backends.django.DjangoTemplates",
+            "DIRS": STATICFILES_DIRS,
+            "APP_DIRS": True,
+            "OPTIONS": {
+                "context_processors": [
+                    "django.template.context_processors.debug",
+                    "django.template.context_processors.request",
+                    "django.contrib.auth.context_processors.auth",
+                    "django.contrib.messages.context_processors.messages",
+                ],
+            },
+>>>>>>> Stashed changes
         },
     },
 ]
 
+<<<<<<< Updated upstream
 WSGI_APPLICATION = 'MusicHub.wsgi.application'
 
+=======
+    WSGI_APPLICATION = "MusicHub.wsgi.application"
+>>>>>>> Stashed changes
 
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+<<<<<<< Updated upstream
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+=======
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.postgresql_psycopg2",
+            "NAME": "postgres",
+            "USER": "postgres",
+            "PASSWORD": "postgres",
+            "HOST": "localhost",
+            "PORT": "5432",
+        }
+>>>>>>> Stashed changes
     }
 }
 
+<<<<<<< Updated upstream
+=======
+    # Custom user app
+    AUTH_USER_MODEL = "users.User"
+>>>>>>> Stashed changes
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
 
+<<<<<<< Updated upstream
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
@@ -114,10 +213,59 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
+=======
+    AUTH_PASSWORD_VALIDATORS = [
+        {
+            "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
+        },
+        {
+            "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
+        },
+        {
+            "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
+        },
+        {
+            "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
+        },
+    ]
+
+    # Internationalization
+    # https://docs.djangoproject.com/en/4.1/topics/i18n/
+
+    LANGUAGE_CODE = "en-us"
+
+    TIME_ZONE = "UTC"
+    USE_I18N = False
+    USE_L10N = True
+    USE_TZ = True
+    LOGIN_REDIRECT_URL = "/"
+>>>>>>> Stashed changes
 
 STATIC_URL = 'static/'
 
+<<<<<<< Updated upstream
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+=======
+    DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+    # Django Rest Framework
+    REST_FRAMEWORK = {
+        "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+        "PAGE_SIZE": int(os.getenv("DJANGO_PAGINATION_LIMIT", 10)),
+        "DATETIME_FORMAT": "%Y-%m-%dT%H:%M:%S%z",
+        "DEFAULT_RENDERER_CLASSES": (
+            "rest_framework.renderers.JSONRenderer",
+            "rest_framework.renderers.BrowsableAPIRenderer",
+        ),
+        "DEFAULT_PERMISSION_CLASSES": [
+            "rest_framework.permissions.IsAuthenticated",
+        ],
+        "DEFAULT_AUTHENTICATION_CLASSES": (
+            "rest_framework.authentication.SessionAuthentication",
+            "rest_framework.authentication.TokenAuthentication",
+        ),
+    }
+>>>>>>> Stashed changes
--- MusicHub/users/models.py
+++ MusicHub/users/models.py
@@ -1,3 +1,74 @@
+from uuid import uuid4
 from django.db import models
+from django.core.validators import (
+    RegexValidator,
+    EmailValidator,
+    validate_image_file_extension,
+)
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    BaseUserManager,
+    PermissionsMixin,
+)
 
-# Create your models here.
+
+class CustomManager(BaseUserManager):
+    def create_user(self, email, password, first_name, last_name, **kwargs):
+        user = self.model(
+            email=email, first_name=first_name, last_name=last_name, **kwargs
+        )
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+    def create_superuser(self, email, password, first_name, last_name, **kwargs):
+        user = self.model(
+            email=email, first_name=first_name, last_name=last_name, **kwargs
+        )
+        user.set_password(password)
+        user.is_admin = True
+        user.is_staff = True
+        user.save(using=self._db)
+
+        return user
+
+
+class User(AbstractBaseUser):
+    """Custom Abstract User Model"""
+
+    id = models.UUIDField(primary_key=True, default=uuid4, editable=False, unique=True)
+
+    first_name = models.CharField(max_length=25, blank=False, null=False)
+    last_name = models.CharField(max_length=25, blank=False, null=False)
+    email = models.EmailField(
+        verbose_name="email address",
+        unique=True,
+        validators=[EmailValidator(code="Invalid email")],
+    )  # required
+    password = models.CharField(
+        max_length=100,
+        validators=[
+            RegexValidator(regex="^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*\\W).{8,}$")
+        ],
+    )
+    profile_avatar = models.ImageField(
+        "Avatar",
+        upload_to="users/avatar",
+        blank=True,
+        null=True,
+        validators=[validate_image_file_extension],
+    )
+    followers = models.ManyToManyField("users.User", blank=True, symmetrical=False)
+
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    is_admin = models.BooleanField(default=False)
+
+    objects = CustomManager()
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["first_name", "last_name", "password"]
+
+    def __str__(self):
+        return self.email

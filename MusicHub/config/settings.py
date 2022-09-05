76fafe3a5c83fc6198253220d4bc80033975264e
Henok Urufa <heno.urufa@itechart-group.com>
2022-09-05 11:24:29 +0200

    MS-76
    Create User model

--- MusicHub/config/settings.py
+++ MusicHub/config/settings.py
@@ -11,60 +11,15 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
+from os.path import join
+from distutils.util import strtobool
+from configurations import Configuration
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 
-<<<<<<< Updated upstream
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-$7tq9as8q!4g=&a=4b12+cbbj&sip6l+orw$+xb4dcy!yw*occ'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
-
-
-# Application definition
-
-INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-]
-
-MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
-]
-
-ROOT_URLCONF = 'MusicHub.urls'
-
-TEMPLATES = [
-    {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
-            ],
-=======
 class Common(Configuration):
     # Build paths inside the project like this: BASE_DIR / 'subdir'.
 
@@ -139,27 +94,14 @@ class Common(Configuration):
                     "django.contrib.messages.context_processors.messages",
                 ],
             },
->>>>>>> Stashed changes
         },
-    },
-]
+    ]
 
-<<<<<<< Updated upstream
-WSGI_APPLICATION = 'MusicHub.wsgi.application'
-
-=======
     WSGI_APPLICATION = "MusicHub.wsgi.application"
->>>>>>> Stashed changes
 
-# Database
-# https://docs.djangoproject.com/en/4.1/ref/settings/#databases
+    # Database
+    # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-<<<<<<< Updated upstream
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-=======
     DATABASES = {
         "default": {
             "ENGINE": "django.db.backends.postgresql_psycopg2",
@@ -169,51 +111,14 @@ DATABASES = {
             "HOST": "localhost",
             "PORT": "5432",
         }
->>>>>>> Stashed changes
     }
-}
 
-<<<<<<< Updated upstream
-=======
     # Custom user app
     AUTH_USER_MODEL = "users.User"
->>>>>>> Stashed changes
 
-# Password validation
-# https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
+    # Password validation
+    # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
 
-<<<<<<< Updated upstream
-AUTH_PASSWORD_VALIDATORS = [
-    {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-    },
-]
-
-
-# Internationalization
-# https://docs.djangoproject.com/en/4.1/topics/i18n/
-
-LANGUAGE_CODE = 'en-us'
-
-TIME_ZONE = 'UTC'
-
-USE_I18N = True
-
-USE_TZ = True
-
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/4.1/howto/static-files/
-=======
     AUTH_PASSWORD_VALIDATORS = [
         {
             "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
@@ -239,16 +144,9 @@ USE_TZ = True
     USE_L10N = True
     USE_TZ = True
     LOGIN_REDIRECT_URL = "/"
->>>>>>> Stashed changes
 
-STATIC_URL = 'static/'
+    STATIC_URL = "static/"
 
-<<<<<<< Updated upstream
-# Default primary key field type
-# https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
-
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-=======
     DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
     # Django Rest Framework
@@ -268,4 +166,3 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
             "rest_framework.authentication.TokenAuthentication",
         ),
     }
->>>>>>> Stashed changes

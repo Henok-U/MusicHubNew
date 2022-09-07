f10d2160d1cf42c810753deef176f9dd444ef356
Damian Bednarek <Damian.Bednarek@itechart-group.com>
2022-09-07 14:02:43 +0200

    final fixes

--- MusicHub/main/views.py
+++ MusicHub/main/views.py
@@ -1,19 +1,19 @@
 from django.shortcuts import render
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, parser_classes
 from rest_framework.response import Response
-
+from rest_framework.parsers import JSONParser
 from MusicHub.users.models import User
 from MusicHub.users.custom_storage import MediaStorage
-
+import os
 @api_view(['GET'])
+@parser_classes([JSONParser])
 def is_server_working(request):
+    variant = os.getenv('DJANGO_CONFIGURATION')
     try:
         User.objects.all()
-        media_storage = MediaStorage()
-        media_storage.open('https://musichubstorage.s3.eu-central-1.amazonaws.com/diagnostic/gandalf.jpg')
-        return Response(status=200, data={
-            "Database is working, \n AWS server and storage is working \n Everything looks good"})
-    except FileNotFoundError:
-        return Response(status=404, data={"Error while connecting to S3 storage"})
+        if variant == 'Production':
+            media_storage = MediaStorage()
+            media_storage.open('diagnostic/gandalf.jpg')
+        return Response(status=200, data="Everything looks good!")
     except Exception as e:
             return Response(status=500, data=str(e))

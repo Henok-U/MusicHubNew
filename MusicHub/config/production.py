571b1d00219746f5cb88125a54f686d91c23150e
Damian Bednarek <Damian.Bednarek@itechart-group.com>
2022-09-07 12:46:53 +0200

    Configure to use AWS S3 storage

--- MusicHub/config/production.py
+++ MusicHub/config/production.py
@@ -5,6 +5,18 @@ import os
 class Production(Common):
     INSTALLED_APPS = Common.INSTALLED_APPS
     SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
+    INSTALLED_APPS += ("storages", )
     # Site
     # https://docs.djangoproject.com/en/2.0/ref/settings/#allowed-hosts
-    ALLOWED_HOSTS = ["*"]
+    ALLOWED_HOSTS = ["*"] 
+    STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+    AWS_ACCESS_KEY_ID = os.getenv('DJANGO_AWS_ACCESS_KEY_ID')
+    AWS_SECRET_ACCESS_KEY = os.getenv('DJANGO_AWS_SECRET_ACCESS_KEY')
+    AWS_STORAGE_BUCKET_NAME = os.getenv('DJANGO_AWS_STORAGE_BUCKET_NAME')
+    
+
+    DEFAULT_FILE_STORAGE = 'MusicHub.users.custom_storage.MediaStorage'
+    AWS_STORAGE_BUCKET_NAME = "musichubstorage"
+    AWS_DEFAULT_ACL = None
+    MEDIA_URL = f'https://musichubstorage.s3.eu-central-1.amazonaws.com/'
+    

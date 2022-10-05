efd90b2e67cc9ff08bec3d9e7085eb9a3db5879a
Henok <henokmekonenpl@gmail.com>
2022-10-05 14:33:09 +0200

    Update models.py
    Replaced on_delete.CASCADE with on_delete.SET()

--- MusicHub/tracks/models.py
+++ MusicHub/tracks/models.py
@@ -1,9 +1,11 @@
-from django.db import models
-from django.core.validators import RegexValidator, FileExtensionValidator
 import os
-from ..config.settings import Common
-
 from uuid import uuid4
+
+from django.contrib.auth import get_user_model
+from django.core.validators import FileExtensionValidator, RegexValidator
+from django.db import models
+
+from ..config.settings import Common
 
 
 def get_upload_path(instance, filename):
@@ -12,6 +14,11 @@ def get_upload_path(instance, filename):
         instance.created_by.get_email_short(),
         filename,
     )
+
+
+def get_sentinal_user():
+    deleted_user = get_user_model().objects.get_or_create(email="deleted_user")
+    return deleted_user[0]  # id is on index 0 by default
 
 
 class Track(models.Model):
@@ -24,16 +31,18 @@ class Track(models.Model):
         null=False,
         validators=[RegexValidator(regex="^[a-zA-Z0-9][a-zA-Z0-9\s\,\.\-]*$")],
     )
-    track = models.FileField(
+    file = models.FileField(
         upload_to=get_upload_path,
         blank=False,
         null=False,
         validators=[FileExtensionValidator(["mp3", "wav", "aac"])],
     )
     track_length = models.PositiveIntegerField(blank=True, null=True)
-    public = models.BooleanField(default=True)
+    is_public = models.BooleanField(default=True)
     created_at = models.DateTimeField(auto_now_add=True)
-    created_by = models.ForeignKey(Common.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    created_by = models.ForeignKey(
+        Common.AUTH_USER_MODEL, on_delete=models.SET(get_sentinal_user)
+    )
 
     def __str__(self):
         return self.filename

a62b614944e8e82ae91be4589b12371ba190f1d3
Henok <henokmekonenpl@gmail.com>
2022-10-05 14:31:29 +0200

    Update views.py
    - Removed unnecessarily overridden methods
    - Refactored documentation

--- MusicHub/tracks/views.py
+++ MusicHub/tracks/views.py
@@ -1,7 +1,5 @@
 from django.utils.decorators import method_decorator
-
 from drf_yasg.utils import swagger_auto_schema
-
 from rest_framework import generics, status
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
@@ -46,46 +44,41 @@ class ListTracksView(generics.ListAPIView):
     View for listing tracks owned by user
     """
 
-    def get_queryset(self):
-        tracks = Track.objects.filter(created_by=self.request.user)
-        return tracks.order_by("-created_at")
-
     permission_classes = [IsAuthenticated]
     serializer_class = ListTrackSerializer
     pagination_class = LargeResultsSetPagination
 
+    def get_queryset(self):
+        tracks = Track.objects.filter(created_by=self.request.user)
+        return tracks.order_by("-created_at")
 
+
+@method_decorator(
+    name="delete",
+    decorator=swagger_auto_schema(
+        manual_parameters=[custom_track_schema.TOKEN_PARAMETER],
+        responses=custom_track_schema.basic_response(
+            "200", "Track deleted successfully"
+        ),
+    ),
+)
 class DeleteOneTrackView(generics.DestroyAPIView):
     """
     View for deleting tracks owned by user
     provided id of track
     """
 
+    permission_classes = [IsAuthenticated]
+    serializer_class = ListTrackSerializer
+
     def get_queryset(self):
         track = Track.objects.all()
         return track
 
-    permission_classes = [IsAuthenticated]
-    serializer_class = ListTrackSerializer
-
-    @swagger_auto_schema(
-        manual_parameters=[
-            custom_track_schema.TOKEN_PARAMETER,
-        ],
-        responses=custom_track_schema.basic_response(
-            "200", "Track deleted successfully"
-        ),
-    )
-    def delete(self, request, *args, **kwargs):
-        try:
-            track = self.get_object()
-            self.perform_destroy(track)
-            return Response(
-                {"message": "Track deleted Successfull!"},
-                status=status.HTTP_204_NO_CONTENT,
-            )
-        except Track.DoesNotExist:
-            return Response(
-                {"message": "Unable to delete, Invalid track id!"},
-                status=status.HTTP_404_NOT_FOUND,
-            )
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        instance.delete()
+        return Response(
+            {"detail": "Track deleted Successfull!"},
+            status=status.HTTP_204_NO_CONTENT,
+        )

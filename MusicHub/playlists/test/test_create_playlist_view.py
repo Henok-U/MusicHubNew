98aeede5a9593641f0d094b2ecf5f4852efef922
Henok <henokmekonenpl@gmail.com>
2022-10-12 15:53:21 +0200

    Update test_create_playlist_view.py

--- MusicHub/playlists/test/test_create_playlist_view.py
+++ MusicHub/playlists/test/test_create_playlist_view.py
@@ -8,19 +8,19 @@ from ..models import Playlist
 from ...users.models import User
 
 
-@pytest.mark.django_db
-def test_playlist_create():
-    me = User.objects.create(
-        email="test@example.com", password="abcABC123", is_verified=True
-    )
-    Playlist.objects.create(name="Test Playlist", is_public=False, created_by=me)
+class TestPlaylistView:
+    @pytest.mark.django_db
+    def test_playlist_create(*args):
+        me = User.objects.create(
+            email="test@example.com", password="abcABC123", is_verified=True
+        )
+        Playlist.objects.create(name="Test Playlist", is_public=False, created_by=me)
 
-    assert Playlist.objects.count() == 1
+        assert Playlist.objects.count() == 1
 
-
-@pytest.mark.django_db
-def test_unauthorized(client):
-    url = reverse("create-playlist")
-    response = client.post(url)
-    print(response.data)
-    assert response.status_code == status.HTTP_401_UNAUTHORIZED
+    @pytest.mark.django_db
+    def test_unauthorized(client):
+        url = reverse("create-playlist")
+        response = client.post(url)
+        print(response.data)
+        assert response.status_code == status.HTTP_401_UNAUTHORIZED

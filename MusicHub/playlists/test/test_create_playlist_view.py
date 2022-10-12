ad0d5395efd4870e57cdd331def1a4211e0e41e2
Henok <henokmekonenpl@gmail.com>
2022-10-12 15:28:27 +0200

    Initial test

---
+++ MusicHub/playlists/test/test_create_playlist_view.py
@@ -0,0 +1,26 @@
+from urllib import response
+import pytest
+
+from rest_framework import status
+from rest_framework.reverse import reverse
+
+from ..models import Playlist
+from ...users.models import User
+
+
+@pytest.mark.django_db
+def test_playlist_create():
+    me = User.objects.create(
+        email="test@example.com", password="abcABC123", is_verified=True
+    )
+    Playlist.objects.create(name="Test Playlist", is_public=False, created_by=me)
+
+    assert Playlist.objects.count() == 1
+
+
+@pytest.mark.django_db
+def test_unauthorized(client):
+    url = reverse("create-playlist")
+    response = client.post(url)
+    print(response.data)
+    assert response.status_code == status.HTTP_401_UNAUTHORIZED

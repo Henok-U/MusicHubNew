b3a886190e07b0fa6366609737e554832ec62267
Henok <henokmekonenpl@gmail.com>
2022-10-14 12:34:41 +0200

    Swagger doc

--- MusicHub/playlists/views.py
+++ MusicHub/playlists/views.py
@@ -1,11 +1,19 @@
+from django.utils.decorators import method_decorator
+from drf_yasg.utils import swagger_auto_schema
 from rest_framework.generics import CreateAPIView, UpdateAPIView
-from rest_framework.parsers import MultiPartParser, FileUploadParser
+from rest_framework.parsers import MultiPartParser
 from rest_framework.permissions import IsAuthenticated
+
+from MusicHub.playlists.custom_playlist_schema import TOKEN_PARAMETER
 
 from .models import Playlist
 from .serializers import PlaylistSerializer
 
 
+@method_decorator(
+    name="post",
+    decorator=swagger_auto_schema(manual_parameters=[TOKEN_PARAMETER]),
+)
 class CreatePlaylistView(CreateAPIView):
     """
     Create a new Playlist
@@ -21,6 +29,10 @@ class CreatePlaylistView(CreateAPIView):
         return context
 
 
+@method_decorator(
+    name="patch",
+    decorator=swagger_auto_schema(manual_parameters=[TOKEN_PARAMETER]),
+)
 class UpdatePlaylistView(UpdateAPIView):
     """
     Change the cover image for a playlist
